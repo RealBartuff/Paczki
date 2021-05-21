996f5aa57ad1505aaf7530e59107faef132b5555
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-05-21 18:26:39 +0200

    poprawki wykonane podczas warsztatów

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ waga_paczki = 0
 ilosc_paczek = 0
 ilosc_elementow = 0
 maks_paczka = 20
+suma_kilo = 0
+suma_pustych_kilo = 0
 
 element = int(input())
 while element:
@@ -15,14 +17,21 @@ while element:
             print("Aktualna waga paczki: ", waga_paczki)
             print("Wysłano paczkę o wadze: ", waga_paczki)
         elif waga_paczki > 20:
+            ilosc_paczek += 1
+            waga_paczki -= element
+            suma_pustych_kilo += maks_paczka - waga_paczki
             print("Wysłano paczkę o wadze: ", (waga_paczki - element))
-        elif waga_paczki < 20:
-            waga_paczki += element
-        element = int(input())
+            waga_paczki = element
+        element = input()
+        if element:
+            element = int(element)
     else:
         print("Element za ciężki: ", element)
         break
+print("ilosc paczek: ", ilosc_paczek)
 
+if suma_kilo > 0:
+    ilosc_paczek += 1
 
 """
 if element == 0:
@@ -30,5 +39,7 @@ if element == 0:
 
 else:
     print("Za duża waga elementu")
+
     
 """
+
