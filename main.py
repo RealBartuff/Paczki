165f5cc42970a09c90b1c6a676a2ecc2a0b6e8f9
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-05-21 21:21:45 +0200

    Samodzielne poprawki piątek

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
 
 waga_paczki = 0
-ilosc_paczek = 0
-ilosc_elementow = 0
 maks_paczka = 20
+ilosc_paczek = 0
 suma_kilo = 0
 suma_pustych_kilo = 0
 
-element = int(input())
+element = float(input())
 while element:
     if element <= 10:
-        print("Dodano element do paczki: ", element)
-        ilosc_elementow += 1
-        print("Elementy w paczce: ", ilosc_elementow)
+        element = float(input())
         waga_paczki += element
-        if waga_paczki == 20:
-            print("Aktualna waga paczki: ", waga_paczki)
-            print("Wysłano paczkę o wadze: ", waga_paczki)
-        elif waga_paczki > 20:
+        print("Dodano element do paczki: {}, Akutualna waga paczki: {}".format(element, waga_paczki))
+        if waga_paczki == maks_paczka:
             ilosc_paczek += 1
+            print("Wysłano paczkę o wadze: {}, Wysłane paczki: {}".format(waga_paczki, ilosc_paczek))
+        if waga_paczki > 20:
+            print("Paczka przeciążona! Dodaję element do nowej paczki.")
             waga_paczki -= element
+            ilosc_paczek += 1
+            suma_kilo += waga_paczki
             suma_pustych_kilo += maks_paczka - waga_paczki
-            print("Wysłano paczkę o wadze: ", (waga_paczki - element))
-            waga_paczki = element
-        element = input()
-        if element:
-            element = int(element)
-    else:
-        print("Element za ciężki: ", element)
+            print("Wysłano paczkę o wadze: {}, Puste kilogramy {}".format(waga_paczki, suma_pustych_kilo))
+            waga_paczki = 0
+            if 1 > element > 10:
+                print("Nieprawidłowa waga elementu (podaj wagę z przedziału od 1 do 10 kg)")
+                break
+    elif element == 0:
+        print("Koniec programu")
         break
-print("ilosc paczek: ", ilosc_paczek)
+    else:
+        print("error")
+        break
 
-if suma_kilo > 0:
+'''print("Suma wysłanych paczek: ", ilosc_paczek)
+print("Suma wysłanych kilogramów: ", suma_kilo)
+print("Suma pustych kilogramów: ", suma_pustych_kilo)
+
+elif waga_paczki > 20:
     ilosc_paczek += 1
+    waga_paczki -= element
+    suma_pustych_kilo += maks_paczka - waga_paczki
+    print("Wysłano paczkę o wadze: ", (waga_paczki - element))
+    waga_paczki = element
+element = float(input())
+if element:
+    element = float(element)'''
 
-"""
-if element == 0:
-    print("Koniec programu")
-
-else:
-    print("Za duża waga elementu")
-
-    
-"""
 
